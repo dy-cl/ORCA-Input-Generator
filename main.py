bdbab16150a3f41d06d6953af69897663bf4449d
dylan <dylanrcounsell@gmail.com>
2023-05-28 10:17:18 +0100

    Added UV-vis (Excited State) and removed blank space in file names which was causing problems with ORCA

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ def smiles_to_xyz(smiles):
     builder = ob.OBBuilder()
     builder.Build(mol)
 
+    #Add implicit hydrogens
+    mol.AddHydrogens()
+
     #Write to XYZ format
     xyz_content = obConversion.WriteString(mol)
 
@@ -59,28 +62,35 @@ class Input_Generator:
 
         calculation_statements = {
         'energy': '\n',
-        'optimization': '!OPT\n',
-        'vibration': '!FREQ\n',
-        'vib + freq': '!OPT\n!FREQ\n',
-        'nmr' : '!NMR TightSCF\n' #Use default SCF setting for now
+        'optimization': '! OPT\n',
+        'vibration': '! FREQ\n',
+        'vib + freq': '! OPT\n!FREQ\n',
+        'nmr' : '! NMR TightSCF\n', #Use default SCF setting for now
+        'uv' : '\n'
         }
 
         try:  
             with open(unique_file_name, 'w') as f:
                 f.write(f'#\n'
-                        f'#{file_name}\n' #Input title
+                        f'# {file_name.replace(".inp", "")}\n' #Input title
                         f'#\n'
                         f'%maxcore 3000\n' #Memory to use
                         f'\n')
                 
                 if calculation_type == 'nmr':
-                    f.write(f'!{method} {basis_set} AutoAux\n') #Use automatic auxilliary basis set for NMR maybe add options later
+                    f.write(f'! {method} {basis_set} AutoAux\n') #Use automatic auxilliary basis set for NMR add options later
                 
                 else:
-                    f.write(f'!{method} {basis_set}\n')
+                    f.write(f'! {method} {basis_set}\n')
 
                 f.write(calculation_statements.get(calculation_type, '\n'))
-                
+
+                if calculation_type == 'uv':
+                    f.write(f'%cis\n'  #Use default excited state method and default parameters add options later
+                            f'  nroots 8\n'
+                            f'  maxdim 64\n'
+                            f'end\n')
+
                 f.write(f'\n'
                         f'* {coordinate_type} {charge} {spinmult} \n'
                         f'{xyz_content}'
@@ -99,8 +109,9 @@ class Input_Generator:
     #Energy calculation
     @staticmethod
     def energy_input(method_choice, basis_choice, SMILES, methods, basis_sets, orca_tasks, calculation_choice):
-        SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  # Remove () and []
-        file_name = f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_{SMILES}_{orca_tasks[calculation_choice - 1]}.inp"
+        SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  #Remove () and []
+        file_name = (f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_"
+                     f"{SMILES}_{orca_tasks[calculation_choice - 1].replace(' ', '-')}.inp") #Remove blank space from file name (prevents opening error)
         coordinate_type = 'xyz' #Coordinate type
         calculation_type = 'energy'
         charge = input("Enter the charge (This is typically 0): ") 
@@ -113,8 +124,9 @@ class Input_Generator:
     #Geometry optimization
     @staticmethod
     def optimization_input(method_choice, basis_choice, SMILES, methods, basis_sets, orca_tasks, calculation_choice):
-        SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  # Remove () and []
-        file_name = f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_{SMILES}_{orca_tasks[calculation_choice - 1]}.inp"
+        SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  #Remove () and []
+        file_name = (f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_"
+                     f"{SMILES}_{orca_tasks[calculation_choice - 1].replace(' ', '-')}.inp") #Remove blank space from file name (prevents opening error)
         coordinate_type = 'xyz' #Coordinate type 
         calculation_type = 'optimization'
         charge = input("Enter the charge (This is typically 0): ") 
@@ -128,7 +140,8 @@ class Input_Generator:
     @staticmethod
     def vibrational_input(method_choice, basis_choice, SMILES, methods, basis_sets, orca_tasks, calculation_choice):
         SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  # Remove () and []
-        file_name = f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_{SMILES}_{orca_tasks[calculation_choice - 1]}.inp"
+        file_name = (f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_"
+                     f"{SMILES}_{orca_tasks[calculation_choice - 1].replace(' ', '-')}.inp") #Remove blank space from file name (prevents opening error)
         coordinate_type = 'xyz' #Coordinate type 
         calculation_type = 'vibration'
         charge = input("Enter the charge (This is typically 0): ") 
@@ -142,7 +155,8 @@ class Input_Generator:
     @staticmethod
     def opt_freq_input(method_choice, basis_choice, SMILES, methods, basis_sets, orca_tasks, calculation_choice):
         SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  # Remove () and []
-        file_name = f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_{SMILES}_{orca_tasks[calculation_choice - 1]}.inp"
+        file_name = (f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_"
+                     f"{SMILES}_{orca_tasks[calculation_choice - 1].replace(' ', '-')}.inp") #Remove blank space from file name (prevents opening error)
         coordinate_type = 'xyz' #Coordinate type 
         calculation_type = 'vib + freq'
         charge = input("Enter the charge (This is typically 0): ") 
@@ -156,9 +170,25 @@ class Input_Generator:
     @staticmethod
     def nmr_input(method_choice, basis_choice, SMILES, methods, basis_sets, orca_tasks, calculation_choice):
         SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  # Remove () and []
-        file_name = f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_{SMILES}_{orca_tasks[calculation_choice - 1]}.inp"
+        file_name = (f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_"
+                     f"{SMILES}_{orca_tasks[calculation_choice - 1].replace(' ', '-')}.inp") #Remove blank space from file name (prevents opening error)
         coordinate_type = 'xyz' #Coordinate type 
         calculation_type = 'nmr'
+        charge = input("Enter the charge (This is typically 0): ") 
+        spin = int(input("Enter the spin (This is typically 0): ")) 
+        xyz_content = smiles_to_xyz(SMILES)
+
+        Input_Generator.write_input_file(file_name, methods[method_choice - 1], basis_sets[basis_choice - 1], 
+                                         coordinate_type, charge, spin, xyz_content, calculation_type)
+        
+    #Excited states and UV-vis calculation
+    @staticmethod
+    def UV_input(method_choice, basis_choice, SMILES, methods, basis_sets, orca_tasks, calculation_choice):
+        SMILES = SMILES.replace('(', '').replace(')', '').replace('[', '').replace(']', '')  # Remove () and []
+        file_name = (f"{methods[method_choice - 1]}_{basis_sets[basis_choice - 1]}_"
+                     f"{SMILES}_{orca_tasks[calculation_choice - 1].replace(' ', '-')}.inp") #Remove blank space from file name (prevents opening error)
+        coordinate_type = 'xyz' #Coordinate type 
+        calculation_type = 'uv'
         charge = input("Enter the charge (This is typically 0): ") 
         spin = int(input("Enter the spin (This is typically 0): ")) 
         xyz_content = smiles_to_xyz(SMILES)
@@ -168,7 +198,7 @@ class Input_Generator:
 
 #Main function
 def main():
-    orca_tasks = ["Energy", "Geometry Optimization", "Vibrational Frequencies", "Optimize + Vib Freq", "NMR"]
+    orca_tasks = ["Energy", "Geometry Optimization", "Vibrational Frequencies", "Optimize and Vib-Freq", "NMR", "UV-vis and Excited state"]
     methods = ['B3LYP', 'HF', 'MP2', 'CCSD']
     basis_sets = ['STO-3G', '3-21G', '6-31G(d)', 'def2-SVP']
 
@@ -180,7 +210,8 @@ def main():
         2: Input_Generator.optimization_input,
         3: Input_Generator.vibrational_input,
         4: Input_Generator.opt_freq_input,
-        5: Input_Generator.nmr_input
+        5: Input_Generator.nmr_input,
+        6: Input_Generator.UV_input
     }
 
     if calculation_choice in calculation_functions:
